d5608b72a723ce1a676130fc86b433fb21120a9f
Ana Julia Silverio <ana.silverio@zup.com.br>
2023-06-23 12:19:56 -0300

    adicionando verificao do cartão de crédito

--- GitLabWorkflow/src/main/java/Model/PagamentoCartaoCredito.java
+++ GitLabWorkflow/src/main/java/Model/PagamentoCartaoCredito.java
@@ -3,11 +3,13 @@ package Model;
 public class PagamentoCartaoCredito implements Pagamento{
 
     private String numeroCartao;
-    private int cvv;
+    private String cvv;
     private double preco;
 
 
-    public PagamentoCartaoCredito(String numeroCartao,int cvv) {
+    public PagamentoCartaoCredito(String numeroCartao,String cvv) {
+        if (!validaNumeroCartao(numeroCartao)) throw new IllegalArgumentException("Numero de cartão invalido");
+        if (!validarCVV(cvv)) throw new IllegalArgumentException("Numero Cvv inválido");
         this.numeroCartao = numeroCartao;
         this.cvv = cvv;
     }
@@ -20,5 +22,48 @@ public class PagamentoCartaoCredito implements Pagamento{
     public double getPreco() {
         return preco;
 
+    }
+    public boolean validaNumeroCartao(String numeroCartao){
+        // Verificar se o número do cartão possui 16 dígitos
+        if (numeroCartao.length() != 16) {
+            return false;
+        }
+
+        // Verificar se o número do cartão contém apenas dígitos
+        if (!numeroCartao.matches("\\d+")) {
+            return false;
+        }
+
+        // Verificar usando o algoritmo de Luhn
+        int sum = 0;
+        boolean doubleDigit = false;
+        for (int i = numeroCartao.length() - 1; i >= 0; i--) {
+            int digit = Character.getNumericValue(numeroCartao.charAt(i));
+
+            if (doubleDigit) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit = digit % 10 + 1;
+                }
+            }
+
+            sum += digit;
+            doubleDigit = !doubleDigit;
+        }
+
+        return sum % 10 == 0;
+    }
+    public static boolean validarCVV(String cvv) {
+        // Verificar se o CVV possui 3 dígitos
+        if (cvv.length() != 3) {
+            return false;
+        }
+
+        // Verificar se o CVV contém apenas dígitos
+        if (!cvv.matches("\\d+")) {
+            return false;
+        }
+
+        return true;
     }
 }
